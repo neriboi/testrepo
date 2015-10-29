b0e9e78420085408a81a3f8a06a1c1347596a2d8
neriboi <neriboi@gmail.com>
2015-10-29 16:09:27 +0800

    v8 tim
    removed rootScope at MSApp

--- js/app2.js
+++ js/app2.js
@@ -27,7 +27,7 @@ angular.module('demoService', []).factory('DemoService', function($rootScope){
     return service;
 });
 
-angular.module('MsApp', ['ngRoute', 'MsControllers', 'demoService', 'ngAnimate', 'ui.bootstrap', '$rootScope'])
+angular.module('MsApp', ['ngRoute', 'MsControllers', 'demoService', 'ngAnimate', 'ui.bootstrap'])
   .run(function () {
 		window.fbAsyncInit = function () {
 			FB.init({
