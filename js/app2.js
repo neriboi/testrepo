624b826e4b1cd55403f0ec32bcb131acc27ecc69
neriboi <neriboi@gmail.com>
2016-01-22 16:16:13 +0800

    v6

--- js/app2.js
+++ js/app2.js
@@ -1,4 +1,4 @@
-angular.module('MsApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'ngCookies'])
+angular.module('MsApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'ngCookies', 'chieffancypants.loadingBar', 'ngAnimate'])
   .run(function () {
 		window.fbAsyncInit = function () {
 			FB.init({
@@ -51,7 +51,8 @@ angular.module('MsApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'ngCookies'])
 		return service;
 	})
   
-  .controller('ExampleController', ['$scope', '$http', 'DemoService', '$cookies', function($scope, $http, DemoService, $cookies) {
+  .controller('ExampleController', ['$scope', '$http', 'DemoService', '$cookies', function($scope, $http, DemoService, $cookies, cfpLoadingBar) {
+	
 	$scope.buttonLabel = DemoService.buttonLabel;
 	$scope.bLogged = DemoService.bLogged;
 	$scope.searchLoc = DemoService.Loc.SearchLoc;
@@ -278,7 +279,7 @@ angular.module('MsApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'ngCookies'])
    }])
    
    .controller("ListController", ['$scope','$http', '$routeParams', 'DemoService', '$log', 
-	function($scope, $http, $routeParams, DemoService, $log)
+	function($scope, $http, $routeParams, DemoService, $log, cfpLoadingBar)
 		{
 			var sLoc = DemoService.Loc.SearchLoc;
 			var data = '{"sLocation":"' + DemoService.Loc.SearchLoc + '","sSearch":"' + $routeParams.categoryName + '"}';
@@ -315,7 +316,7 @@ angular.module('MsApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'ngCookies'])
 	)
 	
 	.controller("DetailsController", ['$scope', '$http', '$routeParams', 'DemoService', '$log', '$cookies',
-	function($scope, $http, $routeParams, DemoService, $log, $cookies)
+	function($scope, $http, $routeParams, DemoService, $log, $cookies, cfpLoadingBar)
 		{
 			var aJSON = [];
 			if (DemoService.Post[$routeParams.postID].iReviews > 0) {
@@ -399,7 +400,7 @@ angular.module('MsApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'ngCookies'])
 	)
 	
 	.controller("searchController", ['$scope','$http', '$routeParams', 'DemoService',
-	function($scope, $http, $routeParams, DemoService)
+	function($scope, $http, $routeParams, DemoService, cfpLoadingBar)
 		{
 			var sLoc = DemoService.Loc.SearchLoc;
 			var data = '{"sLocation":"' + $routeParams.searchLoc + '","sSearch":"' + $routeParams.searchQuery + '"}';
@@ -433,11 +434,9 @@ angular.module('MsApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'ngCookies'])
 			);
 		}]
 	)
-   
-	//
 	
 	.controller("MainController", ['$scope', 'DemoService', '$http', '$log',
-	function($scope, DemoService, $http, $log)
+	function($scope, DemoService, $http, $log, cfpLoadingBar)
 		{
 			var sLoc = DemoService.Loc.SearchLoc;
 			var data = '{"sLocation":"' + DemoService.Loc.SearchLoc + '"}';
@@ -480,11 +479,236 @@ angular.module('MsApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'ngCookies'])
 			$scope.myInterval = 5000;
 			$scope.noWrapSlides = false;
 			$scope.searchLoc = sLoc;
-			$scope.slides = [{image: 'images/carousel1.png'}, {image: 'images/carousel2.png'}, {image: 'images/carousel3.png'}];
+			$scope.slides = [{image: 'images/carousel1-min.png'}, {image: 'images/carousel2-min.png'}, {image: 'images/carousel3-min.png'}];
 		}]
 	)
 	
-	//
+	.controller("addController", ['$scope', '$http', '$routeParams', 'DemoService', '$cookies',
+	function($scope, $http, $routeParams, DemoService, $cookies, cfpLoadingBar)
+		{
+			var userToken = $cookies.get('myToken');
+			var userName = $cookies.get('myName');
+			$scope.logged = $cookies.get('myLogged');
+			
+			var mainCats = ['Party and Wedding Services', 'Professional Services', 'Health and Wellness Services', 'Properties and Rentals Services', 'Travel Services', 'Home Services'];
+			var Cats = [
+				[{id: 0,  mainCat: 'Party and Wedding Services',  subCat: 'Bridal Hair and Make Up'},
+				{id: 1,  mainCat: 'Party and Wedding Services',  subCat: 'Catering'},
+				{id: 2,  mainCat: 'Party and Wedding Services',  subCat: 'Costume'},
+				{id: 3,  mainCat: 'Party and Wedding Services',  subCat: 'Event Planning and Organisers'},
+				{id: 4,  mainCat: 'Party and Wedding Services',  subCat: 'Floral'},
+				{id: 5,  mainCat: 'Party and Wedding Services',  subCat: 'Hall Rentals'},
+				{id: 6,  mainCat: 'Party and Wedding Services',  subCat: 'Invitation Designing'},
+				{id: 7,  mainCat: 'Party and Wedding Services',  subCat: 'Lights and Sounds'},
+				{id: 8,  mainCat: 'Party and Wedding Services',  subCat: 'Orchestra and Music'},
+				{id: 9,  mainCat: 'Party and Wedding Services',  subCat: 'Photography and Video'},
+				{id: 10,  mainCat: 'Party and Wedding Services',  subCat: 'Wedding Attire'}],
+				[{id: 11,  mainCat: 'Professional Services',  subCat: 'Accounting'},
+				{id: 12,  mainCat: 'Professional Services',  subCat: 'Computer and IT'},
+				{id: 13,  mainCat: 'Professional Services',  subCat: 'Courier and Distribution'},
+				{id: 14,  mainCat: 'Professional Services',  subCat: 'Engineering'},
+				{id: 15,  mainCat: 'Professional Services',  subCat: 'Legal'},
+				{id: 16,  mainCat: 'Professional Services',  subCat: 'Photography and Video'},
+				{id: 17,  mainCat: 'Professional Services',  subCat: 'Security'},
+				{id: 18,  mainCat: 'Professional Services',  subCat: 'Training'}],
+				[{id: 19,  mainCat: 'Health and Wellness Services',  subCat: 'Anesthesiology'},
+				{id: 20,  mainCat: 'Health and Wellness Services',  subCat: 'Cardiology'},
+				{id: 21,  mainCat: 'Health and Wellness Services',  subCat: 'Dental'},
+				{id: 22,  mainCat: 'Health and Wellness Services',  subCat: 'Dermatology'},
+				{id: 23,  mainCat: 'Health and Wellness Services',  subCat: 'Family and Internal Medicine'},
+				{id: 24,  mainCat: 'Health and Wellness Services',  subCat: 'Fitness and Personal Care'},
+				{id: 25,  mainCat: 'Health and Wellness Services',  subCat: 'Gastroenterology'},
+				{id: 26,  mainCat: 'Health and Wellness Services',  subCat: 'General Surgery'},
+				{id: 27,  mainCat: 'Health and Wellness Services',  subCat: 'Geriatrics'},
+				{id: 28,  mainCat: 'Health and Wellness Services',  subCat: 'Hematology and Oncology'},
+				{id: 29,  mainCat: 'Health and Wellness Services',  subCat: 'Hospitals and Clinics'},
+				{id: 30,  mainCat: 'Health and Wellness Services',  subCat: 'Medical Supplies and Drugstores'},
+				{id: 31,  mainCat: 'Health and Wellness Services',  subCat: 'Neurology'},
+				{id: 32,  mainCat: 'Health and Wellness Services',  subCat: 'Obstetrics and Gynecology'},
+				{id: 33,  mainCat: 'Health and Wellness Services',  subCat: 'Orthopedic Surgery'},
+				{id: 34,  mainCat: 'Health and Wellness Services',  subCat: 'Otolaryngology'},
+				{id: 35,  mainCat: 'Health and Wellness Services',  subCat: 'Pediatrics'},
+				{id: 36,  mainCat: 'Health and Wellness Services',  subCat: 'Psychiatry'},
+				{id: 37,  mainCat: 'Health and Wellness Services',  subCat: 'Pulmonolgy'},
+				{id: 38,  mainCat: 'Health and Wellness Services',  subCat: 'Urology'}],
+				[{id: 39,  mainCat: 'Properties and Rentals Services',  subCat: 'Apartments and Flats'},
+				{id: 40,  mainCat: 'Properties and Rentals Services',  subCat: 'Commercial Shops'},
+				{id: 41,  mainCat: 'Properties and Rentals Services',  subCat: 'Industrial Space'},
+				{id: 42,  mainCat: 'Properties and Rentals Services',  subCat: 'Office Space'},
+				{id: 43,  mainCat: 'Properties and Rentals Services',  subCat: 'Residential Houses'},
+				{id: 44,  mainCat: 'Properties and Rentals Services',  subCat: 'Vacant Lot'},
+				{id: 45,  mainCat: 'Properties and Rentals Services',  subCat: 'Villas and Beach House'},
+				{id: 46,  mainCat: 'Properties and Rentals Services',  subCat: 'Virtual Office'}],
+				[{id: 47,  mainCat: 'Travel Services',  subCat: 'Gasoline Stations'},
+				{id: 48,  mainCat: 'Travel Services',  subCat: 'Groceries and Convenience Stores'},
+				{id: 49,  mainCat: 'Travel Services',  subCat: 'Lodging'},
+				{id: 50,  mainCat: 'Travel Services',  subCat: 'Pasalubong Centers'},
+				{id: 51,  mainCat: 'Travel Services',  subCat: 'Restaurants and Cafes'},
+				{id: 52,  mainCat: 'Travel Services',  subCat: 'Transport'},
+				{id: 53,  mainCat: 'Travel Services',  subCat: 'Travel Agencies'}],
+				[{id: 54,  mainCat: 'Home Services',  subCat: 'Appliance Repair'},
+				{id: 55,  mainCat: 'Home Services',  subCat: 'Cleaning'},
+				{id: 56,  mainCat: 'Home Services',  subCat: 'Demolition'},
+				{id: 57,  mainCat: 'Home Services',  subCat: 'Flooring'},
+				{id: 58,  mainCat: 'Home Services',  subCat: 'Home Construction'},
+				{id: 59,  mainCat: 'Home Services',  subCat: 'Interior Designing'},
+				{id: 60,  mainCat: 'Home Services',  subCat: 'Lawn and Garden'},
+				{id: 61,  mainCat: 'Home Services',  subCat: 'Pest Control'},
+				{id: 62,  mainCat: 'Home Services',  subCat: 'Plumbing'},
+				{id: 63,  mainCat: 'Home Services',  subCat: 'Solar Products and Installation'},
+				{id: 64,  mainCat: 'Home Services',  subCat: 'Waterproofing'}]
+			 ]
+			 
+			 $scope.tempCat = '';
+			 $scope.tempSubCat = '';
+			 
+			 $scope.mainCat = mainCats;
+			 $scope.subCats = [];
+			 $scope.getSubCats = function(){
+				var key = $scope.mainCat.indexOf($scope.tempCat);
+				var myNewOptions = Cats[key];
+				$scope.subCats = myNewOptions;
+			};
+			
+			var mainLocs = ['Metro Manila (NCR)','Metro Cebu (MC)','Metro Davao (MD)','Cordillera Administrative Region (CAR)','Ilocos Region (R1)','Cagayan Valley (R2)','Central Luzon (R3)','CALABARZON (R4A)',
+						'MIMAROPA (R4B)','Bicol Region (R5)','Western Visayas (R6)','Central Visayas (R7)','Eastern Visayas (R8)','Negros Island Region (R18)','Zamboanga Peninsula (R9)','Northern Mindanao (R10)',
+						'Davao Region (R11)','SOCCSKSARGEN (R12)','Caraga (R13)','Autonomous Region Muslim Mindanao (ARMM)'];
+			var subLocs = [['Manila','Caloocan','Las Pinas','Makati','Malabon','Mandaluyong','Marikina','Muntinlupa','Navotas','Paranaque','Pasay','Pasig','Pateros','Quezon City','San Juan','Taguig','Valenzuela'], // 0-17
+						['Carcar City','Cebu City','Compostela','Consolacion','Cordova','Danao City','Lapu-Lapu City','Liloan','Mandaue City','Minglanilla','Naga City','San Fernando','Talisay'], //18-31
+						['Davao City','Digos City','Panabo City','Samal City','Tagum City','Carmen','Santa Cruz'], //32-39
+						['Abra','Apayao','Baguio','Benguet','Ifugao','Kalinga','Mountain Province'], //40-47
+						['Dagupan','Ilocos Norte','Ilocos Sur','La Union','Pangasinan'], //48-53
+						['Batanes','Cagayan','Isabella','Nueva Vizcaya','Quirino','Santiago'], //54-60
+						['Angeles','Aurora','Bataan','Bulacan','Nueva Ecija','Olongapo','Pampanga','Tarlac','Zambales'], //61-70
+						['Batangas','Cavite','Laguna','Lucena','Quezon','Rizal'], //71-77
+						['Marinduque','Occidental Mindoro','Oriental Mindoro','Palawan','Puerto Princesa','Romblon'], //78-84
+						['Albay','Camarines Norte','Camarines Sur','Catanduanes','Masbate','Naga','Sorsogon'], //85-92
+						['Aklan','Antique','Capiz','Guimaras','Iloilo','Iloilo City'], //93-99
+						['Bohol','Cebu','Lapu-Lapu','Mandaue','Siquijor'], //100-105
+						['Biliran','Eastern Samar','Leyte','Northern Samar','Ormoc','Samar','Southern Leyte','Tacloban'], //106-114
+						['Bacolod','Negros Occidental','Negros Oriental'], //115-118
+						['Isabela City','Zamboanga City','Zamboanga del Norte','Zamboanga del Sur','Zamboanga Sibugay'], //119-124
+						['Bukidnon','Cagayan de Oro','Camiguin','Iligan','Lanao del Norte','Misamis Occidental','Misamis Oriental'], //125-132
+						['Compostela Valley','Davao City','Davao del Norte','Davao del Sur','Davao Oriental','Davao Occidental'], //133-139
+						['Cotabato','Cotabato City','General Santos','Sarangani','South Cotabato','Sultan Kudarat'], //140-146
+						['Agusan del Norte','Agusan del Sur','Butuan','Dinagat Islands','Surigao del Norte','Surigao del Sur'], //147-153
+						['Basilan','Lanao del Sur','Maguindanao','Sulu','Tawi-Tawi']]; //154-159
+			
+			$scope.tempRegion = '';
+			$scope.tempCity = '';
+			 
+			$scope.mainLoc = mainLocs;
+			$scope.subLoc = [];
+			$scope.getSubLocs = function(){
+				var lockey = $scope.mainLoc.indexOf($scope.tempRegion);
+				var myLocOptions = subLocs[lockey];
+				$scope.subLoc = myLocOptions;
+			};
+			
+			$scope.upload = '';
+			$scope.image = [];
+			$scope.openFile = function() {
+				var filesSelected = document.getElementById("inputFileToLoad").files;
+				if (filesSelected.length > 0)
+				{
+					var fileToLoad = filesSelected[0];
+
+					var fileReader = new FileReader();
+					var srcData = document.createElement("img");
+					fileReader.onload = function(fileLoadedEvent) {
+						srcData = fileLoadedEvent.target.result // <--- data: base64
+						var tempSrcData = srcData.split(",");
+						$scope.image.push(tempSrcData[1]);
+					}
+					fileReader.readAsDataURL(fileToLoad);
+				}
+			};
+			
+			$scope.details = {
+				sPhone: '',
+				sMobile: '',
+				sEmail: '',
+				sAddress: '',
+				sPerson: '',
+				sOpen: '',
+				sFB: '',
+				sDesc: ''
+			};
+			
+			$scope.post = {
+				sName: '',
+				mainCat: '',
+				subCat: '',
+				aLoc: [],
+				aDetails: []
+			};
+			
+			$scope.addPost = function(){
+			  $scope.post.mainCat = $scope.tempSubCat.mainCat;
+			  $scope.post.subCat = $scope.tempSubCat.subCat;
+			  $scope.post.aLoc.push($scope.tempRegion);
+			  $scope.post.aLoc.push($scope.tempCity);
+			  $scope.post.aDetails.push($scope.details.sPhone);
+			  $scope.post.aDetails.push($scope.details.sMobile);
+			  $scope.post.aDetails.push($scope.details.sEmail);
+			  $scope.post.aDetails.push($scope.details.sAddress);
+			  $scope.post.aDetails.push($scope.details.sPerson);
+			  $scope.post.aDetails.push($scope.details.sOpen);
+			  $scope.post.aDetails.push($scope.details.sFB);
+			  $scope.post.aDetails.push($scope.details.sDesc);
+			  
+			  var data = '{"sName":"' + $scope.post.sName + '","sCategory":"' + $scope.post.mainCat + '","sSubcategory":"' + $scope.post.subCat + '","aLocation":["' + $scope.post.aLoc.join('","') + '"],"aDetails":["' + $scope.post.aDetails.join('","') + '"],"aPhotos":["' + $scope.image.join('","') + '"]}';
+			  var dEncoded = btoa(data);
+			  var objectToSerialize={'sEncoded':dEncoded};
+			
+			  $http({
+				method: 'POST', 
+				url: 'https://api.parse.com/1/functions/setServices', 
+				headers: { 'X-Parse-Application-Id':'9XYZMrEUVyTb2VJM4zOuW3cxEyOAAnPSwnkFDURM', 'X-Parse-REST-API-Key':'HoW440iQCWQFVT6qW2qpo0wrVflSq7bH8VTQjOeV', 'X-Parse-Session-Token':userToken},
+				data: objectToSerialize
+			  }).success(function(data)
+				{
+					var aData = atob(data.result);
+					console.log(aData);
+					var stringURL = 'index.html#/myposts';
+					location.href = stringURL;
+				}
+			  );
+			};
+		}]
+	)
+	
+	.controller("myPostsController", ['$scope','$http', '$routeParams', 'DemoService', '$cookies', 
+	function($scope, $http, $routeParams, DemoService, $cookies, cfpLoadingBar)
+		{
+			var userToken = $cookies.get('myToken');
+			var userName = $cookies.get('myName');
+			$scope.logged = $cookies.get('myLogged');
+			
+			$http({
+				method: 'POST', 
+				url: 'https://api.parse.com/1/functions/getMyPosts', 
+				headers: { 'X-Parse-Application-Id':'9XYZMrEUVyTb2VJM4zOuW3cxEyOAAnPSwnkFDURM', 'X-Parse-REST-API-Key':'HoW440iQCWQFVT6qW2qpo0wrVflSq7bH8VTQjOeV', 'X-Parse-Session-Token':userToken}
+			}).success(function(data)
+				{
+					var aData = atob(data.result).split(';');
+					var aJSON = [];
+					
+					if (aData[0] == 'Error' && aData.length == 2) {
+					
+					} else {
+						for (var x=0; x<aData.length; x++) {
+							var jData = JSON.parse(aData[x]);
+							aJSON.push(jData);
+						}
+					}
+					
+					$scope.posts = aJSON; // response data 
+					DemoService.updateValue($scope.posts);
+				}
+			);
+		}]
+	)
    
    .config(function($routeProvider) {
   $routeProvider
@@ -528,6 +752,30 @@ angular.module('MsApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'ngCookies'])
   .when('/search/:searchLoc/:searchQuery', {
     templateUrl: 'partials/search.html',
     controller: 'searchController',
+    resolve: {
+      // I will cause a 1 second delay
+      delay: function($q, $timeout) {
+        var delay = $q.defer();
+        $timeout(delay.resolve, 1000);
+        return delay.promise;
+      }
+    }
+  })
+  .when('/add/', {
+    templateUrl: 'partials/add.html',
+    controller: 'addController',
+    resolve: {
+      // I will cause a 1 second delay
+      delay: function($q, $timeout) {
+        var delay = $q.defer();
+        $timeout(delay.resolve, 1000);
+        return delay.promise;
+      }
+    }
+  })
+  .when('/myposts', {
+    templateUrl: 'partials/myposts.html',
+    controller: 'myPostsController',
     resolve: {
       // I will cause a 1 second delay
       delay: function($q, $timeout) {
